b9cf0fd77c1f686d14e5ae5653a9105fb72f759d
Diyorbek Sultanov <diyorbeksultanov.ss@gmail.com>
2026-04-17 16:13:26 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-ADMIN_IDS: list[int] = [
-    int(x.strip()) for x in os.getenv("ADMIN_IDS", "0").split(",") if x.strip()
-]
